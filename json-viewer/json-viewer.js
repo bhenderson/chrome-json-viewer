c7ccde629b8f036fd65a34f8d5805f91311d21af
Brian Henderson <henderson.bj@gmail.com>
2017-01-31 06:06:09 -0800

    Initial commit.

---
+++ json-viewer/json-viewer.js
@@ -0,0 +1,151 @@
+javascript:(
+  function (){
+    var collapsible = function(event) {
+      var div = this.querySelector("div");
+      if (!div) return;
+      event.stopPropagation();
+      if (window.getSelection().toString()) return;
+
+      this.isHidden = !this.isHidden;
+      if (this.isHidden) {
+        this.className = "collapsible hidden";
+      } else {
+        this.className = "collapsible";
+      }
+      while (div.nodeName === "DIV") {
+        div.hidden = this.isHidden;
+        div = div.nextSibling;
+      }
+    };
+
+    var loop = function(obj, callback) {
+      if (Array.isArray(obj)) {
+        var len = obj.length - 1;
+        obj.forEach(function(val, idx) {
+          callback(val, idx, idx === len);
+        });
+        return;
+      }
+      if (typeof obj === "object") {
+        var keys = Object.keys(obj);
+        var len = keys.length - 1;
+        keys.forEach(function(key, idx) {
+          var val = obj[key];
+          callback(val, key, idx === len);
+        });
+        return;
+      }
+    };
+
+    var create = function(el, props) {
+      var e = document.createElement(el);
+      if (props) {
+        Object.keys(props).forEach(function(key) {
+          e[key] = props[key];
+        });
+      }
+      e.inner_text = function(obj) {
+        return e.inner(document.createTextNode(obj));
+      };
+      e.inner = function(obj) {
+        e.appendChild(obj);
+        return e;
+      };
+      e.addTail = function(add) {
+        if (add) e.inner_text(",");
+        return e;
+      };
+      e.loop = function(obj, callback) {
+        var innerLoop = function(val, key, last) {
+          e.inner(callback(val, key, last));
+        };
+        loop(obj, innerLoop);
+        return e;
+      };
+
+      if (e.tagName === "DIV") {
+        e.onclick = collapsible.bind(e);
+      }
+
+      return e;
+    };
+
+    var createArrayElem = function(val, key, last) {
+      return create("div")
+        .inner(parse(val))
+        .addTail(!last);
+    };
+
+    var createObjectElem = function(val, key, last) {
+      return create("div")
+        .inner(parse(key, "key"))
+        .inner_text(":  ")
+        .inner(parse(val))
+        .addTail(!last);
+    };
+
+    var parse = function(obj, klass) {
+      if (!klass) var klass = ("type-" + (obj === null ? "null" : typeof obj));
+      var span = create("span", {className: klass});
+
+      if (Array.isArray(obj)) {
+        span
+          .inner_text("[")
+          .loop(obj, createArrayElem)
+          .inner_text("]");
+        return span;
+      }
+
+      if (typeof obj === "object" && obj !== null) {
+        span
+          .inner_text("{")
+          .loop(obj, createObjectElem)
+          .inner_text("}");
+        return span;
+      }
+
+      span.onclick = function(e) {
+        e.stopPropagation();
+      };
+
+      if (typeof obj === "string" && obj.match("^https?://")) {
+        var alink = create("a", {className: klass, href: obj})
+          .inner_text(obj);
+        span
+          .inner_text('"')
+          .inner(alink)
+          .inner_text('"');
+      } else {
+        span
+          .inner_text(JSON.stringify(obj));
+      }
+
+      return span;
+    };
+
+    document.head.innerHTML = "" +
+    "  <style>                 " +
+    "    div {                 " +
+    "      margin-left: 10px;  " +
+    "    }                     " +
+    "    .type-boolean {       " +
+    "      color: red;         " +
+    "    }                     " +
+    "    .type-string {        " +
+    "      color: green;       " +
+    "    }                     " +
+    "    .type-number {        " +
+    "      color: blue;        " +
+    "    }                     " +
+    "    .type-null {          " +
+    "      color: gray;        " +
+    "    }                     " +
+    "  </style>                " +
+    "";
+
+    window.json = JSON.parse(document.body.innerText);
+    var data = create("div").inner(parse(window.json));
+    document.body.innerHTML = "";
+    document.body.appendChild(data);
+  }
+)()
